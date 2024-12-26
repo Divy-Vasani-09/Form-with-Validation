e1e41152cc304976901f6f50d93f6a1d675c7792
Divy-Vasani-09 <divyvasani.niqox@gmail.com>
2024-12-26 11:43:38 +0530

    Chenge all onChange event handler to one onChange event handler for all input

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -33,64 +33,18 @@ export default function Registration() {
     "Canada",
     "Australia",
   ];
-  const [year, setYear] = useState('');
-  const [month, setMonth] = useState('');
-  const [day, setDay] = useState('');
 
   const showErrorInBorder = 'border-red-700';
   const unShowErrorInBorder = 'border-slate-950';
 
-
-  const handleFNOnChange = (e) => {
-    setInputValues(inV => ({ ...inV, FullName: e.target.value.replace(/\s{2}/, ' ').replace(/[^A-Za-z\s]/ig, '') }))
-  }
-
-  const handleUDOnChange = (e) => {
-    setInputValues(inV => ({ ...inV, UserName: e.target.value.replace(/\s/g, '').replace(/[^A-Za-z_0-9]/ig, '').replace(/_{2}/g, '_') }))
-  }
-
-  const handleEmlOnChange = (e) => {
-    setInputValues(inV => ({ ...inV, EmailID: e.target.value.replace(/\s/g, '').replace(/[^a-z0-9.@]/ig, '').replace(/@{2}/, '@').replace(/[.]{2}/, '.') }))
-  }
-
-  const handleMNOnChange = (e) => {
-    setInputValues(inV => ({...inV, PhoneNo: e.target.value.replace(/\s/g, '').replace(/[^0-9]/g, '')}))
-  }
-
-
-  const handleCountryChange = (e) => {
-    setInputValues(inV => ({...inV, CountryName: e.target.value}))
-    const optionS = document.getElementById('optionS').value;
-    console.log(optionS)
-  }
-
-  const handlePassOnChange = (e) => {
-    setInputValues(inV=> ({...inV, Password: e.target.value}))
-  }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputValues({
+      ...inputValues,
+      [name]: value
+    });
+  };
   const pwd = inputValues.Password;
-
-  const handleConPassOnChange = (e) => {
-    setInputValues(inV=>({...inV, ConfirmPassword: e.target.value}))
-  }
-
-  const handleDateChange = (e) => {
-    const newDate = e.target.value;
-    setInputValues(inV =>({...inV, BirthDate: newDate}))
-    if (inputValues.BirthDate) {
-      const dateParts = inputValues.BirthDate.split('-');
-      const yearValue = dateParts[0];
-      const monthValue = dateParts[1];
-      const dayValue = dateParts[2];
-
-      setYear(yearValue);
-      setMonth(monthValue);
-      setDay(dayValue);
-    } else {
-      setYear('');
-      setMonth('');
-      setDay('');
-    }
-  }
 
   const handleValidations = () => {
     if (!fullNRegex.test(inputValues.FullName)) {
@@ -106,36 +60,36 @@ export default function Registration() {
       return
     }
     if (!mobileNRegex.test(inputValues.PhoneNo)) {
-      setInputValuesErr({PhoneNoErr: true})
+      setInputValuesErr({ PhoneNoErr: true })
       return
     }
     const optionS = document.getElementById('optionS').value;
     if (inputValues.CountryName === '' || inputValues.CountryName === optionS) {
-      setInputValuesErr({CountryNameErr: true})
+      setInputValuesErr({ CountryNameErr: true })
       return
     }
     if (!passRegex.test(inputValues.Password)) {
-      setInputValuesErr({PasswordErr: true})
+      setInputValuesErr({ PasswordErr: true })
       return
     }
     if (pwd !== inputValues.ConfirmPassword) {
-      setInputValuesErr({ConfirmPasswordErr: true})
+      setInputValuesErr({ ConfirmPasswordErr: true })
       return
     }
     if (inputValues.Gender == '') {
-      setInputValuesErr({GenderErr: true})
+      setInputValuesErr({ GenderErr: true })
       return
     }
     if (inputValues.BirthDate == '') {
-      setInputValuesErr({BirthDateErr: true})
+      setInputValuesErr({ BirthDateErr: true })
       return
     }
-    else{
-      setInputValuesErr({BirthDateErr: false})
-      
+    else {
+      setInputValuesErr({ BirthDateErr: false })
+
       let getData = JSON.parse(localStorage.getItem("inputValues")) || [];
       getData.push(inputValues);
-      localStorage.setItem('dataV', JSON.stringify(getData));
+      localStorage.setItem('inputValues', JSON.stringify(getData));
       console.log(JSON.stringify(getData));
       console.log(inputValues);
     }
@@ -149,9 +103,10 @@ export default function Registration() {
           <input
             type="text"
             placeholder="Full Name"
+            name='FullName'
             value={inputValues.FullName}
             required
-            onChange={handleFNOnChange}
+            onChange={handleChange}
             className={`p-2 py-1 mx-5 my-1 w-3/4 border-2 border-solid ${inputValuesErr.FullNameErr ? showErrorInBorder : unShowErrorInBorder} rounded-lg bg-slate-950`}
           />
           {inputValuesErr.FullNameErr && <p className='text-red-700'>Your user name is invalid</p>}
@@ -161,9 +116,10 @@ export default function Registration() {
           <input
             type="text"
             placeholder="User Name"
+            name='UserName'
             value={inputValues.UserName}
             required
-            onChange={handleUDOnChange}
+            onChange={handleChange}
             className={`p-2 py-1 mx-5 my-1 w-3/4 border-2 border-solid ${inputValuesErr.UserNameErr ? showErrorInBorder : unShowErrorInBorder} rounded-lg bg-slate-950`}
           />
           {inputValuesErr.UserNameErr && <p className='text-red-700 text-'>Your user name is invalid</p>}
@@ -173,9 +129,10 @@ export default function Registration() {
           <input
             type="email"
             placeholder="Email Id"
+            name='EmailID'
             value={inputValues.EmailID}
             required
-            onChange={handleEmlOnChange}
+            onChange={handleChange}
 
             className={`p-2 py-1 mx-5 my-1 w-3/4 border-2 border-solid ${inputValuesErr.EmailIDErr ? showErrorInBorder : unShowErrorInBorder} rounded-lg bg-slate-950`}
           />
@@ -186,10 +143,11 @@ export default function Registration() {
           <input
             type="text"
             placeholder="Phone No."
+            name='PhoneNo'
             value={inputValues.PhoneNo}
             maxLength="10"
             required
-            onChange={handleMNOnChange}
+            onChange={handleChange}
             className={`p-2 py-1 mx-5 my-1 w-3/4 border-2 border-solid ${inputValuesErr.PhoneNoErr ? showErrorInBorder : unShowErrorInBorder} rounded-lg bg-slate-950`}
           />
           {inputValuesErr.PhoneNoErr && <p className='text-red-700'>Your Phone No. is invalid </p>}
@@ -198,7 +156,8 @@ export default function Registration() {
         <div className=" country-section  text-center ">
           <select
             required
-            onChange={handleCountryChange}
+            name='CountryName'
+            onChange={handleChange}
             className={`p-1 py-1 my-1 w-3/4 text-base  text-slate-400 border-2 border-solid ${inputValuesErr.CountryNameErr ? showErrorInBorder : unShowErrorInBorder} rounded-lg bg-slate-950 cursor-pointer`}
           >
             <option id='optionS'>Please choose Country</option>
@@ -217,9 +176,10 @@ export default function Registration() {
           <input
             type="text"
             placeholder="Password"
+            name='Password'
             value={inputValues.Password}
             maxLength="16"
-            onChange={handlePassOnChange}
+            onChange={handleChange}
             className={`px-2 py-1 mx-5 my-1 w-3/4 border-2 border-solid ${inputValuesErr.PasswordErr ? showErrorInBorder : unShowErrorInBorder} rounded-lg bg-slate-950`}
           />
           {inputValuesErr.PasswordErr && <p className='text-red-700 px-3'>Enter at least one UpperCase, LowerCase, Digit and any Symbol</p>}
@@ -229,26 +189,27 @@ export default function Registration() {
           <input
             type="text"
             placeholder="Confirm Password"
+            name='ConfirmPassword'
             value={inputValues.ConfirmPassword}
-            onChange={handleConPassOnChange}
+            onChange={handleChange}
             className={`px-2 py-1 mx-5 my-1 w-3/4 border-2 border-solid ${inputValuesErr.ConfirmPasswordErr ? showErrorInBorder : unShowErrorInBorder} rounded-lg bg-slate-950`}
           />
           {inputValuesErr.ConfirmPasswordErr && <p className='text-red-700'>Enter same password</p>}
         </div>
 
-        <div className="container gender  px-2 py-0 my-1">
+        <div className="container Gender  px-2 py-0 my-1">
           <div className='flex justify-center space-x-2 text-slate-400' >
             <h5 className='font-bold '>Gender:</h5>
             <label className='cursor-pointer flex '>
-              <input type="radio" name="myRadio" id='gender' value="male" className='cursor-pointer text-slate-800' onClick={() => setInputValues(inV=>({...inV,Gender:'male'}))} />
+              <input type="radio" name="Gender" id='gender' value="male" className='cursor-pointer text-slate-800' checked={inputValues.Gender === "male"} onChange={handleChange} />
               <p className='px-1'>Male</p>
             </label>
             <label className='cursor-pointer flex'>
-              <input type="radio" name="myRadio" id='gender' value="female" className='cursor-pointer' onClick={() => setInputValues(inV=>({...inV,Gender:'female'}))} />
+              <input type="radio" name="Gender" id='gender' value="female" className='cursor-pointer' checked={inputValues.Gender === "female"} onChange={handleChange} />
               <p className='px-1'>Female</p>
             </label>
             <label className='cursor-pointer flex'>
-              <input type="radio" name="myRadio" id='gender' value="other" className='cursor-pointer' onClick={() => setInputValues(inV=>({...inV,Gender:'other'}))} />
+              <input type="radio" name="Gender" id='gender' value="other" className='cursor-pointer' checked={inputValues.Gender === "other"} onChange={handleChange} />
               <p className='px-1'>Other</p>
             </label>
           </div>
@@ -259,14 +220,13 @@ export default function Registration() {
           <input
             type="date"
             id="birthDate"
-            name="birthDate"
+            name="BirthDate"
             value={inputValues.BirthDate}
-            onChange={handleDateChange}
+            onChange={handleChange}
             className={`px-2 py-1 mx-5 my-1 w-3/4 border-2 border-solid text-slate-400 ${inputValuesErr.BirthDateErr ? showErrorInBorder : unShowErrorInBorder} rounded-lg bg-slate-950 cursor-pointer`}
           />
           {inputValuesErr.BirthDateErr && <p className='text-red-700 '>Select Your Birth Date</p>}
         </div>
-
 
         <div className='text-center items-center m-3'>
           <button type='submit' onClick={handleValidations} className='text-slate-300  font-bold bg-slate-800 hover:bg-slate-700 p-1 px-4 rounded-lg '>Submit </button>
