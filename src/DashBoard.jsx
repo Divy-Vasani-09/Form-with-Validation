7009b2f9da08b3ee1a825a04dca42099cc9bce08
Divy-Vasani-09 <divyvasani.niqox@gmail.com>
2024-12-27 11:28:38 +0530

    DashBoard component added

---
+++ src/DashBoard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashBoard() {
+  return (
+    <div className='container font-bold text-center text-white'>
+      I am DashBoard
+    </div>
+  )
+}
